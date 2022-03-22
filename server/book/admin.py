ed6d0a080f7492f0c9aa17f93c92bab66c9c7ba4
KenajNazcok <86240942+KenajNazcok@users.noreply.github.com>
2022-03-22 19:09:22 +0100

    create book model

--- server/book/admin.py
+++ server/book/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "publication_date",
+        "isbn_number",
+        "number_of_pages",
+        "cover_link",
+        "publication_language",
+    ]
+    list_filter = ("publication_date",)
+    search_fields = [
+        "title",
+        "author",
+        "publication_language",
+        "publication_date",
+    ]

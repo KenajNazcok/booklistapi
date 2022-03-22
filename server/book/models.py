ad1587e8a6a3aaf8993df9c7098cae8523cf21c2
KenajNazcok <86240942+KenajNazcok@users.noreply.github.com>
2022-03-22 19:09:22 +0100

    create book model

--- server/book/models.py
+++ server/book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField(blank=True, null=True)
+    isbn_number = models.PositiveBigIntegerField(
+        blank=True, null=True, unique=True
+    )
+    number_of_pages = models.PositiveSmallIntegerField(blank=True, null=True)
+    cover_link = models.URLField()
+    publication_language = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title}"
